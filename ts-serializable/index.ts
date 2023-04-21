a106dfa52846c0007ccdd0d7fa6f8bd5ead06a02
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-04-22 02:19:15 +0300

    add ts

---
+++ ts-serializable/index.ts
@@ -0,0 +1,79 @@
+import { jsonObject, jsonProperty, jsonName, Serializable, SnakeCaseNamingStrategy } from "ts-serializable";
+
+@jsonObject({ namingStrategy: new SnakeCaseNamingStrategy() })
+export class GGlmuJ5 extends Serializable {
+  @jsonName("jXPrGl0")
+  @jsonProperty([PQDOf], OUoyn)
+  public d?: [PQDOf] | OUoyn = new OUoyn();
+
+  @jsonName("nuXb")
+  @jsonProperty(PQDOf, [PQDOf], [String])
+  private bJNBrV3y?: PQDOf | [PQDOf] | [string] = new PQDOf();
+
+  @jsonName("raatP")
+  @jsonProperty([PQDOf], PQDOf)
+  private fsedziuU5nH?: [PQDOf] | PQDOf = [new PQDOf()];
+
+  public r(): number {return void 0;};
+
+  private b(): string {return void 0;};
+
+  private faX92(): string {return void 0;};
+
+}
+
+@jsonObject({ namingStrategy: new KebabCaseNamingStrategy() })
+export class OUoyn extends Serializable {
+  @jsonName("jCaYD")
+  @jsonProperty([void 93])
+  private u1LnrLT?: [void 93] = [void 93];
+
+  @jsonName("qFk")
+  @jsonProperty([PQDOf], Date, [Number])
+  private hOnXtUWZ?: [PQDOf] | Date | [number] = [-68];
+
+  @jsonName("jObo")
+  @jsonProperty(PQDOf)
+  public dWdkFaIzb?: PQDOf = new PQDOf();
+
+  @jsonName("b")
+  @jsonProperty(Date)
+  public xR6JSzDKc: Date = new Date("2056-10-18 16:34:24 UTC");
+
+  private bnupK4Q(): number {return void 0;};
+
+  public w8XB0oVIU(): string {return void 0;};
+
+  public ksTn8ro1(): string {return void 0;};
+
+  private xaJ(): string {return void 0;};
+
+}
+
+@jsonObject({ namingStrategy: new KebabCaseNamingStrategy() })
+export class PQDOf extends Serializable {
+  @jsonName("d")
+  @jsonProperty(YHPNW7N, [YHPNW7N])
+  public pYbw5aX?: YHPNW7N | [YHPNW7N] = [new YHPNW7N()];
+
+  @jsonName("cTPLS")
+  @jsonProperty(YHPNW7N, [YHPNW7N], [String])
+  public pBKeYn7PV: YHPNW7N | [YHPNW7N] | [string] = [new YHPNW7N()];
+
+  @jsonName("shPoJTpY")
+  @jsonProperty(YHPNW7N)
+  private a9?: YHPNW7N = new YHPNW7N();
+
+  @jsonName("k53Kl")
+  @jsonProperty([void -97], [YHPNW7N], YHPNW7N)
+  public jSppWqfl: [void -97] | [YHPNW7N] | YHPNW7N = [void -97];
+
+  @jsonName("pWdexZ0CQ7A")
+  @jsonProperty([YHPNW7N])
+  public ir6LM4r: [YHPNW7N] = [new YHPNW7N()];
+
+  public pcyAnJu(): number {return void 0;};
+
+  private rLNu(): number {return void 0;};
+
+}
